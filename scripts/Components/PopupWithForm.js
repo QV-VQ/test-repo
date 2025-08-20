3479584c3769d13c8b638559949dbd240d3ec4b6
QV-VQ <104873694+QV-VQ@users.noreply.github.com>
2025-08-20 03:46:16 -0600

    "feat: complete Card class with like/delete functionality and image orientation detection"

--- scripts/Components/PopupWithForm.js
+++ scripts/Components/PopupWithForm.js
@@ -1,20 +1,36 @@
-import Popup from "./Popup.js";
+import Popup from './Popup.js';
 
 export default class PopupWithForm extends Popup {
-  constructor(popupSelector, submit) {
+  constructor(popupSelector, handleFormSubmit) {
     super(popupSelector);
-    this._submit = submit;
+    this._handleFormSubmit = handleFormSubmit;
+    this._form = this._popup.querySelector('.popup__form');
+    this._inputList = this._form.querySelectorAll('.popup__input');
+    this._submitButton = this._form.querySelector('.popup__button-submit');
   }
 
-  _getInputValues() {}
+  _getInputValues() {
+    const formValues = {};
+    this._inputList.forEach(input => {
+      formValues[input.name] = input.value;
+    });
+    return formValues;
+  }
 
   setEventListeners() {
-    super();
-    // You must add a submit event handler and a click event listener to the form to close the icon.
+    super.setEventListeners();
+    this._form.addEventListener('submit', (evt) => {
+      evt.preventDefault();
+      this._handleFormSubmit(this._getInputValues());
+    });
   }
 
   close() {
-    super();
-    // Wipe form.
+    super.close();
+    this._form.reset();
+  }
+
+  setSubmitButtonText(text) {
+    this._submitButton.textContent = text;
   }
 }
