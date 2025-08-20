e7f25dbee389cebb090b4da3889d50bb4d9761ba
QV-VQ <104873694+QV-VQ@users.noreply.github.com>
2025-08-20 03:52:26 -0600

    "feat: create Section class for dynamic content rendering and management"

--- scripts/Components/Card.js
+++ scripts/Components/Card.js
@@ -1,61 +1,60 @@
-class Card {
-  constructor({ article, handleCardClick }, templateSelector) {
-    this._title = article.title;
-    this._imageUrl = article.imageUrl || article.imageSrc;
-    this._imageAlt = article.imageAlt || article.title;
-    this._iconUrl = article.iconUrl || "../images/heart.svg";
-    this._likedIconUrl = article.likedIconUrl || "../images/heart-liked.svg";
-    this._isLiked = article.isLiked || false;
+export default class Card {
+  constructor({ data, handleCardClick, handleDeleteClick, handleLikeClick }, templateSelector) {
+    this._data = data;
     this._handleCardClick = handleCardClick;
-    this._templateSelector = templateSelector || "#card-template";
+    this._handleDeleteClick = handleDeleteClick;
+    this._handleLikeClick = handleLikeClick;
+    this._templateSelector = templateSelector;
   }
 
-  _getCardFromTemplate() {
-    const cardTemplate = document.querySelector(this._templateSelector).content;
-    this._cardElement = cardTemplate.querySelector(".card").cloneNode(true);
-  }
-
-  _setCardInformation() {
-    this._cardElement.querySelector(".card__place-title").textContent = this._title;
-
-    this._cardImage = this._cardElement.querySelector(".card__image");
-    this._cardImage.src = this._imageUrl;
-    this._cardImage.alt = this._imageAlt ?? this._title;
-    this._cardImage.title = this._title;
-
-    const cardLikeIcon = this._cardElement.querySelector(".card__like-icon");
-    cardLikeIcon.src = this._iconUrl ?? "./images/heart.svg";
-    cardLikeIcon.alt = "like icon";
-    cardLikeIcon.dataset.isLiked = this._isLiked;
-    cardLikeIcon.title = "Click in order to like or dislike";
-
-    const cardDeleteIcon = this._cardElement.querySelector(".card__delete-icon");
-    cardDeleteIcon.src = "./images/delete.svg";
-    cardDeleteIcon.alt = "Delete icon";
-    cardDeleteIcon.dataset.isLiked = this._isLiked;
-    cardDeleteIcon.title = "Click in order to delete card";
-  }
-
-  _defineImageOrientation() {
-    this._cardImage.onload = () => {
-      this._imgOrientation = this._cardImage.naturalWidth > this._cardImage.naturalHeight ? "horizontal" : "vertical";
-      this._cardImage.dataset.orientation = this._imgOrientation;
-    };
+  _getTemplate() {
+    return document
+      .querySelector(this._templateSelector)
+      .content
+      .querySelector('.card')
+      .cloneNode(true);
   }
 
   _setEventListeners() {
-    this._cardElement.addEventListener("click", (evt) => {
-      this._handleCardClick(evt);
+    this._likeButton.addEventListener('click', () => {
+      this._handleLikeClick(this);
+    });
+
+    this._deleteButton.addEventListener('click', () => {
+      this._handleDeleteClick(this);
+    });
+
+    this._cardImage.addEventListener('click', () => {
+      this._handleCardClick(this._data);
     });
   }
 
-  create() {
-    this._getCardFromTemplate();
-    this._setCardInformation();
-    this._defineImageOrientation();
+  updateLikeStatus(isLiked) {
+    this._isLiked = isLiked;
+    this._likeIcon.src = isLiked ? './images/heart-liked.svg' : './images/heart.svg';
+    this._likeIcon.alt = isLiked ? 'Unlike this card' : 'Like this card';
+  }
+
+  removeCard() {
+    this._element.remove();
+    this._element = null;
+  }
+
+  generateCard() {
+    this._element = this._getTemplate();
+    this._likeButton = this._element.querySelector('.card__like-button');
+    this._likeIcon = this._element.querySelector('.card__like-icon');
+    this._deleteButton = this._element.querySelector('.card__delete-button');
+    this._cardImage = this._element.querySelector('.card__image');
+    this._cardTitle = this._element.querySelector('.card__place-title');
+
+    this._cardImage.src = this._data.imageUrl;
+    this._cardImage.alt = this._data.imageAlt;
+    this._cardTitle.textContent = this._data.title;
+
+    this.updateLikeStatus(this._data.isLiked);
     this._setEventListeners();
-    return this._cardElement;
+
+    return this._element;
   }
 }
-
-export default Card;
