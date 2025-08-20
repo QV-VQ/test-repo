c45d5e463d03e6108fbe730da44bd4e1ca2aca73
QV-VQ <104873694+QV-VQ@users.noreply.github.com>
2025-08-20 03:43:39 -0600

    "feat: implement complete popup functionality with proper event handling and ESC key support"

--- scripts/Components/PopupWithImage.js
+++ scripts/Components/PopupWithImage.js
@@ -1,7 +1,16 @@
-import Popup from "./Popup.js";
+import Popup from './Popup.js';
 
 export default class PopupWithImage extends Popup {
   constructor(popupSelector) {
     super(popupSelector);
+    this._imageElement = this._popup.querySelector('.popup__image');
+    this._captionElement = this._popup.querySelector('.popup__figcaption');
+  }
+
+  open(imageSrc, imageAlt) {
+    this._imageElement.src = imageSrc;
+    this._imageElement.alt = imageAlt;
+    this._captionElement.textContent = imageAlt;
+    super.open();
   }
 }
