8ca8a68046db33573ed70fad6e2398eacc5a783b
QV-VQ <104873694+QV-VQ@users.noreply.github.com>
2025-08-20 03:51:26 -0600

    "feat: add UserInfo class for profile data management with local storage support"

--- scripts/Components/Section.js
+++ scripts/Components/Section.js
@@ -2,19 +2,16 @@ export default class Section {
   constructor({ items, renderer }, containerSelector) {
     this._items = items;
     this._renderer = renderer;
-    this._sectionContainer = document.querySelector(containerSelector);
+    this._container = document.querySelector(containerSelector);
   }
 
   renderItems() {
-    this.clear();
-    this._items.forEach((item) => this._renderer(item));
+    this._items.forEach(item => {
+      this._renderer(item);
+    });
   }
 
-  clear() {
-    this._sectionContainer.innerHTML = "";
-  }
-
-  addItem(item) {
-    this._sectionContainer.prepend(item);
+  addItem(element) {
+    this._container.prepend(element);
   }
 }
